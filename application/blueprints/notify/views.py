a0beeaac41cf57e163bdef5d4aa9518e5fe49374
Colm Britton <colmjude@gmail.com>
2016-11-15 11:38:23 +0000

    route for notify product page

--- application/blueprints/notify/views.py
+++ application/blueprints/notify/views.py
@@ -8,3 +8,7 @@ notify = Blueprint('notify', __name__, url_prefix='/notify')
 @notify.route('/register')
 def register_account():
   return render_template('notify_create_account.html')
+
+@notify.route('/productpage')
+def product_page():
+  return render_template('product_page.html')
