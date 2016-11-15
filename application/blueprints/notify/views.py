bf9e6e38c8f6815217afe99f130e5ba668d217d8
Colm Britton <colmjude@gmail.com>
2016-11-15 11:41:42 +0000

    move notify templates into own dir

--- application/blueprints/notify/views.py
+++ application/blueprints/notify/views.py
@@ -7,8 +7,8 @@ notify = Blueprint('notify', __name__, url_prefix='/notify')
 
 @notify.route('/register')
 def register_account():
-  return render_template('notify_create_account.html')
+  return render_template('notify/create_account.html')
 
 @notify.route('/productpage')
 def product_page():
-  return render_template('product_page.html')
+  return render_template('notify/product_page.html')
