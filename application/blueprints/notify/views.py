feb97b8babf291508dd4f7cdae19886995085c2c
Colm Britton <colmjude@gmail.com>
2016-12-06 10:40:09 +0000

    put together flow with azure screenshots"

--- application/blueprints/notify/views.py
+++ application/blueprints/notify/views.py
@@ -1,7 +1,8 @@
 from flask import (
   render_template,
   Blueprint,
-  current_app)
+  current_app,
+  url_for)
 
 notify = Blueprint('notify', __name__, url_prefix='/notify')
 
@@ -20,3 +21,13 @@ def pre_broker():
 @notify.route('/post-signout')
 def post_sign_out():
   return render_template('notify/post_sign_out.html')
+
+@notify.route('/azure/productpage')
+def azure_product_page():
+  return render_template('notify/product_page.html',
+          next_url=url_for('notify.azure_pre_broker'))
+
+@notify.route('/azure/prebroker')
+def azure_pre_broker():
+  return render_template('notify/pre_broker.html',
+          next_url=url_for('broker.azure_request_email'))
